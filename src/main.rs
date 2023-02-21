4ce10d48453ad9c1a6c8e9c3b2f3ad81f312bdff
Gary <121616196+GaryThisSidee@users.noreply.github.com>
2023-02-21 20:27:48 +0530

    Fibonacci number generator completed

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::io;
+fn main(){
+    let mut a=String::new();
+    io::stdin()
+     .read_line(&mut a)
+     .expect("Enter a vaild number");
+    let a=a.trim().parse().expect("Enter a valid number");
+    let mut y=0;
+    let mut i=0;
+    let mut r=1;
+    let mut total;
+    while y!=a{
+       println!("{i}");
+       total=i+r;
+       i=r;
+       r=total;
+       y+=1;
+    }
+}
